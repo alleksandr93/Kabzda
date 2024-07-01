33f417464344a4729d1fc68959d20831815b2945
Aleksandr <aleksandr-ishutin93@mail.ru>
2024-07-01 04:34:59 +0400

    add input.stories.tsx and work with Uncontrolled input

---
+++ src/stories/input.stories.tsx
@@ -0,0 +1,28 @@
+import {Simulate} from 'react-dom/test-utils';
+import input = Simulate.input;
+import {ChangeEvent, useRef, useState} from 'react';
+
+
+export default {
+    title: 'input',
+
+}
+export const UncontrolledInput = () => <input/>
+export const TrackValueOfUncontrolledInput = () => {
+    const [value, setValue] = useState('')
+    const onChange=(e:ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value)
+    return <><input onChange={onChange}/>--{value}</>
+}
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+
+    }
+    return <><input ref={inputRef}/>
+        <button onClick={save}>save</button>
+        actual value:{value}</>
+}
+export const ControlledInputWithFixedValue = () => <input value={'IT_INCUBATOR.BY'}/>
